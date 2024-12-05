fba7ebd2e3dba3025702e24fd2a183d320e733b5
Throm333 <81387131+Throm333@users.noreply.github.com>
2024-12-05 03:59:38 +0100

    Table added
    erste idee vom wochenplaner(in hässlich)

--- src/screens/Weekplanerscreen.js
+++ src/screens/Weekplanerscreen.js
@@ -8,6 +8,56 @@ const Weekplanerscreen = () => {
   );
 };
 
+const Weekplanner = () => {
+  return (
+    <body>
+      <table>
+        <thead>
+          <tr>
+            <th>MO</th>
+            <th>DI</th>
+            <th>MI</th>
+            <th>DO</th>
+            <th>FR</th>
+            <th>SA</th>
+            <th>SO</th>
+          </tr>
+        </thead>
+        <tbody>
+         
+          <tr>
+            <td>Früh</td>
+            <td>Früh</td>
+            <td>Früh</td>
+            <td>Früh</td>
+            <td>Früh</td>
+            <td>Früh</td>
+            <td>Früh</td>
+          </tr>
+          <tr>
+            <td>Mittag</td>
+            <td>Mittag</td>
+            <td>Mittag</td>
+            <td>Mittag</td>
+            <td>Mittag</td>
+            <td>Mittag</td>
+            <td>Mittag</td>
+          </tr>
+          <tr>
+            <td>Abend</td>
+            <td>Abend</td>
+            <td>Abend</td>
+            <td>Abend</td>
+            <td>Abend</td>
+            <td>Abend</td>
+            <td>Abend</td>
+          </tr>
+        </tbody>
+      </table>
+    </body>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
