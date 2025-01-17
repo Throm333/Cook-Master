7da702745d7d2e877f6274a9bcf2b88efa85b89d
Tyler Lababidi <81828536+TylerLababidi@users.noreply.github.com>
2025-01-17 03:40:44 +0100

    Rezeptbild hinzufügen

--- src/screens/AddRecipesScreen.js
+++ src/screens/AddRecipesScreen.js
@@ -1,38 +1,67 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Image, Button, TextInput, ScrollView } from "react-native";
+import * as ImagePicker from 'expo-image-picker'; // Um ein Bild auzuwählen
 
 
 const CreateRecipescreen = () => {
   const [newRecipe, setNewRecipe] = useState("");
   const [portions, setPortions] = useState(2); // Startwert für die Portionenanzahl
+  const [image, setImage] = useState(null); // Speichert den URI des ausgewählten Bildes
+
+  // Funktion um ein Bild aus der Galerie auswählen zu können
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1, // 1 = Maximale Qualität
+    });
+    console.log(result);
+
+    if (!result.canceled) {           // Wenn der User ein Bild auswählt
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.scrollContainer}>
+
+      <Text style={styles.title}>Recipe Title</Text>
       <View style={styles.titleBox}>
-      
         <TextInput                    // Rezepttitel
           style={styles.titleInput}
-          placeholder="Recipe titel"
+          placeholder="What is your recipe called?"
           placeholderTextColor="#888"
           value={newRecipe.title}
           onChangeText={setNewRecipe}
         />
       </View>
 
+
+      <Text style={styles.title}>Recipe Image</Text>
       <View style={styles.imageContainer}> 
         <Image
-          style={styles.recipeImage}
+          style={image ? styles.recipeImage : styles.recipeAddImage}  // 
+          source={image ? { uri: image } : require('../../assets/images/add-image.png')} // Platzhalter wird solange gezeigt bis ein eigenes Bild hochgeladen wird
         />
+        {!image && (<Button title="Choose Image" onPress={pickImage} /> // Der Button existiert nur mit dem Platzhalter
+        )}
       </View>
 
+      {image && (
+        <View style={styles.buttonContainer}>
+          <Button title="Choose Image" onPress={pickImage} />
+        </View>
+      )}
+      
+      <Text style={styles.title}>Serving size</Text>
       <View style={styles.portionContainer}>   
-        <Text style={styles.portionLabel}>Portionen:</Text>
+        <Text style={styles.portionLabel}>Portions:</Text>
         <View style={styles.portionControls}>
           <Button title="-" onPress={() => setPortions(portions > 1 ? portions - 1 : 1)} />
           <TextInput
             style={styles.portionsInput}
             value={String(portions)} // Der Wert des TextInput ist mit `portions` verbunden
-            keyboardType="numeric"
+            keyboardType="numeric"  // Man kann nur Zahlen eintragen
             onChangeText={(value) => {
               const numericValue = Number(value);
               setPortions(numericValue > 0 ? numericValue : ""); // Setze nur positive Werte, sonst leeres Feld
@@ -42,17 +71,28 @@ const CreateRecipescreen = () => {
         </View>
       </View>
 
+      <Text style={styles.title}>Ingredients</Text>
+      <View style={styles.ingredientContainer}>
+          
+      </View>
+
+
+      <Text style={styles.title}>Instructions</Text>
+      <View style={styles.instructionContainer}>
+          
+      </View>
+
         <Button                       // Button um das Rezept zu speichern
           style={styles.saveButton}
           title= "Save Recipe"
           color= "#234a91"
         />
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  scrollContainer: {
     flex: 1,
     backgroundColor: "#fff",
     padding: 16,
@@ -62,9 +102,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     borderRadius: 8,
-    borderWidth: 1, // Create border
-    paddingHorizontal: 10,
-    paddingVertical: 8,
+    borderWidth: 1,
+    padding: 10,
     marginBottom: 16,
     shadowColor: "#000",
     shadowOpacity: 0.1,
@@ -79,28 +118,46 @@ const styles = StyleSheet.create({
     color: "#000",
   },
 
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+
   imageContainer: {       // Rezeptbild Container
     alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    borderWidth: 1,
     marginBottom: 16,
     width: "100%",
     height: 300,
+    overflow: 'hidden', // Damit das Bild nicht außerhalb der Box zu sehen ist
   },
 
-  recipeImage: {      // Platzhalter für das Bild später
-    borderRadius: 8,
-    borderWidth: 2,
+  recipeAddImage: {      // Platzhalter für das Bild später
+    width: 150,
+    height: 150,
+  },
+
+  recipeImage: {      // Rezeptbild
     width: "100%",
-    height: "100%",
+    height: '100%',
+  },
+
+  buttonContainer: {
+    alignItems: "center",
+    marginBottom: 16,
   },
 
   portionContainer: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",  // neu
+    justifyContent: "space-between", 
+    padding: 10,
     marginBottom: 16,
-    padding: 10,  // instead of padding vertical and horizontel
-    borderWidth: 1,
     borderRadius: 8,
+    borderWidth: 1,
     shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 4,
@@ -128,8 +185,26 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
 
+  ingredientContainer: {       // Zubereitung
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+    width: "100%",
+    height: 50,
+  },
+
+  instructionContainer: {       // Zubereitung
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+    width: "100%",
+    height: 50,
+  },
+
 });
 
 export default CreateRecipescreen;
-
-
