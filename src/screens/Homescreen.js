65583a992c60a8fd5cca2b52130ebe45521813c9
zemma753 <saamanthaa48@gmail.com>
2025-01-26 15:19:30 +0100

    Trennung Controller und View

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,52 +1,11 @@
-import React, { useState, useEffect, useCallback } from "react"
+import React from "react"
 import { View, Text, FlatList, TextInput, StyleSheet, TouchableOpacity, Image } from "react-native"
-import supabase from "../data/API_Config"
 import { useNavigation } from "@react-navigation/native"
-import Ionicons from "react-native-vector-icons/Ionicons"
-import { debounce } from "lodash"
+import { HomeController } from "../controller/HomeController"
 
 const HomeScreen = () => {
-  const [recipes, setRecipes] = useState([])
-  const [favorites, setFavorites] = useState({})
-  const [searchQuery, setSearchQuery] = useState("")
-
   const navigation = useNavigation()
-
-  const fetchRecipes = async (query = "") => {
-    try {
-      let supabaseQuery = supabase.from("recipes").select("*").limit(16)
-
-      if (query) {
-        supabaseQuery = supabaseQuery.ilike("title", `%${query}%`)
-      }
-
-      const { data, error } = await supabaseQuery
-      if (error) throw error
-      setRecipes(data || [])
-    } catch (error) {
-      console.error("Fehler beim Abrufen der Rezepte:", error.message)
-    }
-  }
-
-  const debouncedFetchRecipes = useCallback(
-    debounce((query) => fetchRecipes(query), 300),
-    [],
-  )
-
-  useEffect(() => {
-    fetchRecipes()
-  }, [])
-
-  useEffect(() => {
-    debouncedFetchRecipes(searchQuery)
-  }, [searchQuery, debouncedFetchRecipes]) // Added debouncedFetchRecipes to dependencies
-
-  const toggleFavorite = (id) => {
-    setFavorites((prevFavorites) => ({
-      ...prevFavorites,
-      [id]: !prevFavorites[id],
-    }))
-  }
+  const { recipes, favorites, searchQuery, setSearchQuery, toggleFavorite } = HomeController()
 
   const renderItem = ({ item }) => (
     <TouchableOpacity
